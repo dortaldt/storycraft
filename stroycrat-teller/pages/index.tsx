2a834deb7eea29c7714573bcd741931dea6e351f
dortaldt <dortaldt@gmail.com>
2023-04-17 18:51:35 +0000

    created a new json for the content

--- stroycrat-teller/pages/index.tsx
+++ stroycrat-teller/pages/index.tsx
@@ -1,54 +1,37 @@
-import React, { useState } from 'react';
+// Import required modules and components
+import React, { useState, useEffect } from 'react';
 import ParagraphBanner from './ParagraphBanner';
 import CategoryMenu from './CategoryMenu';
 import ImageLoader from './ImageLoader';
 
 function App() {
+  // Default image URL
   const defaultImageUrl = 'https://static.wixstatic.com/media/845144_90be4482dfb04734b9f11bb7bdbfeefe~mv2.png/v1/fill/w_600,h_600,al_c,lg_1,q_85,enc_auto/download__88_-removebg-preview_edited_pn.png';
+  
+  // API endpoint
   const apiEndpoint = 'https://dortaldt-fictional-barnacle-xrr5xqr4xpph6gg7-7860.preview.app.github.dev/';
+  
+  // State for selected category and categories list
   const [selectedCategory, setSelectedCategory] = useState(null);
-  const [categories, setCategories] = useState([
-    {
-      id: 1,
-      title: 'Category 1',
-      description: 'This is a short description of category 1',
-      image: 'https://static.wixstatic.com/media/845144_f3aae4b50c8445a78166af9355075563~mv2.png/v1/fill/w_344,h_344,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/wb23-removebg-preview.png',
-      content: [
-        { type: 'paragraph', value: 'Paragraph 1/nfor Category 1' },
-        { type: 'paragraph', value: 'Paragraph 2 for Category 1' },
-        { type: 'paragraph', value: 'Paragraph 3 for Category 1' },
-        { type: 'imageLoader', value: 'The story starts in your childs room, creating a familiar base for a magical journey. This connection immerses them in the narrative, engaging with characters and plot.'}
-      ],
-      selected: false,
-    },
-    {
-      id: 2,
-      title: 'Category 2',
-      description: 'This is a short description of category 2',
-      image: 'https://static.wixstatic.com/media/845144_e07869a00e394209b07d7efd22ce799a~mv2.png/v1/fill/w_344,h_344,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/wb17.png',
-      content: [
-        { type: 'paragraph', value: 'Paragraph 1 for Category 2' },
-        { type: 'paragraph', value: 'Paragraph 2 for Category 2' },
-        { type: 'paragraph', value: 'Paragraph 3 for Category 2' },
-        { type: 'imageLoader', value: 'ImageLoader description for Category 2' },
-      ],
-      selected: false,
-    },
-    {
-      id: 3,
-      title: 'Category 3',
-      description: 'This is a short description of category 3',
-      image: 'https://via.placeholder.com/150',
-      content: [
-        { type: 'paragraph', value: 'Paragraph 1 for Category 3' },
-        { type: 'paragraph', value: 'Paragraph 2 for Category 3' },
-        { type: 'paragraph', value: 'Paragraph 3 for Category 3' },
-        { type: 'imageLoader', value: 'ImageLoader description for Category 3' },
-      ],
-      selected: false,
-    }
-  ]);
+  const [categories, setCategories] = useState([]);
 
+  // Function to fetch content data from an external JSON file
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch('/categories.json');
+      const data = await response.json();
+      setCategories(data);
+    } catch (error) {
+      console.error('Error fetching categories:', error);
+    }
+  };
+
+  // Call fetchCategories on component mount
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  // Function to handle category selection
   const handleCategorySelect = (category) => {
     const updatedCategories = categories.map((cat) =>
       cat.id === category.id ? { ...cat, selected: true } : { ...cat, selected: false }
@@ -59,7 +42,10 @@ function App() {
 
   return (
     <div className="App">
+      {/* Render CategoryMenu component with required props */}
       <CategoryMenu categories={categories} onCategorySelect={handleCategorySelect} setCategories={setCategories} />
+      
+      {/* Render ParagraphBanner component when a category is selected */}
       {selectedCategory && <ParagraphBanner
         paragraphs={selectedCategory.content}
         imageLoaderTriggerParagraph={3}

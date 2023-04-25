0aeb749043d351604ed5d45dab4cde665dff677c
dortaldt <dortaldt@gmail.com>
2023-04-25 17:31:06 +0000

    added meta tag

--- stroycrat-teller/pages/_document.tsx
+++ stroycrat-teller/pages/_document.tsx
@@ -3,7 +3,28 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link 
+          rel="icon" 
+          sizes="192x192" 
+          href="https://static.wixstatic.com/media/845144_75ea805a78db49e0943a1c96a33b7aca%7Emv2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/845144_75ea805a78db49e0943a1c96a33b7aca%7Emv2.png"
+          />
+        <link rel="shortcut icon" href="https://static.wixstatic.com/media/845144_75ea805a78db49e0943a1c96a33b7aca%7Emv2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/845144_75ea805a78db49e0943a1c96a33b7aca%7Emv2.png" type="image/png"/>
+        <link rel="apple-touch-icon" href="https://static.wixstatic.com/media/845144_75ea805a78db49e0943a1c96a33b7aca%7Emv2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/845144_75ea805a78db49e0943a1c96a33b7aca%7Emv2.png" type="image/png"/>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:site_name" content="StoryCraft"></meta>
+        <meta property="og:url" content="https://www.storycraft.ai"></meta>
+        <meta property="og:image:height" content="1330"></meta>
+        <meta property="og:image:width" content="2500"></meta>
+        <meta property="og:image" content="https://static.wixstatic.com/media/845144_e9cb6a64dc9c42cd890a33530dc42530%7Emv2.png/v1/fit/w_2500,h_1330,al_c/845144_e9cb6a64dc9c42cd890a33530dc42530%7Emv2.png"></meta>
+        <meta property="og:description" content="Crafting beautiful and personalized children's stories using cutting-edge technology."></meta>
+        <meta property="og:title" content="StoryCraft AI"></meta>
+        <meta name="twitter:image" content="https://static.wixstatic.com/media/845144_e9cb6a64dc9c42cd890a33530dc42530~mv2.png"></meta>
+        <meta name="twitter:description" content="Crafting beautiful and personalized children's stories using cutting-edge technology."></meta>
+        <meta name="twitter:title" content="StoryCraft AI"></meta>
+        <meta name="twitter:card" content="summary_large_image"></meta>
+
+      </Head>
       <body>
         <Main />
         <NextScript />

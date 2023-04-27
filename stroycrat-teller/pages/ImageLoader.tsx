ab2b4f81007ed249382bc0d41c11b597b1b21ed2
dortaldt <dortaldt@gmail.com>
2023-04-27 12:18:22 +0000

    loader to the bottom

--- stroycrat-teller/pages/ImageLoader.tsx
+++ stroycrat-teller/pages/ImageLoader.tsx
@@ -145,13 +145,14 @@ const ImageLoader = ({ defaultImageUrl, apiEndpoint, onResponse, loaderDescripti
 
   return (
     <div>
-        <div className={"progress-bar " + 'progbar-container ' + (showProgress ? ' ' : 'transparent') }>
-          <div className="progress-bar-inner" id="progress-bar-inner" style={{width: uploadProgress+ '%'}}></div>
-        </div>
+        
         {/* <div className={'progbar-container ' + (showProgress ? ' ' : 'transparent')}>
           <progress className='progress' value={uploadProgress} max="100" />
         </div> */}
       <img src={imageSrc} alt="Preview" style={{ maxWidth: '100%' }} />
+      <div className={"progress-bar " + 'progbar-container ' + (showProgress ? ' ' : 'transparent') }>
+          <div className="progress-bar-inner" id="progress-bar-inner" style={{width: uploadProgress+ '%'}}></div>
+        </div>
       {showButtonsContainer && (
         <div className='buttons-container'>
           {firstImageUploaded && (
